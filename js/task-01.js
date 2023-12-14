4ea7e0a68f2baa8461fa390f7277b8d06ac8a5ce
Kyryl <MaDeR04eK@users.noreply.github.com>
2023-12-14 17:54:09 +0100

    finished task-2

--- js/task-01.js
+++ js/task-01.js
@@ -2,7 +2,7 @@ const items = document.querySelectorAll('.item');
 console.log(`Number of categories: ${items.length}`);
 
 items.forEach(category => {
-    const categoryName = category.querySelector('h2').textContent;
-    const categoryItem = category.querySelectorAll('li').length;
-    console.log(`Category: ${categoryName}, Elements: ${categoryItem}`);
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryItem = category.querySelectorAll('li').length;
+  console.log(`Category: ${categoryName}, Elements: ${categoryItem}`);
   });
