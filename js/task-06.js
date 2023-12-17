e50a843db425f712be58f5332d82d2479280d653
Kyryl <MaDeR04eK@users.noreply.github.com>
2023-12-17 09:38:59 +0100

    finished task-6

--- js/task-06.js
+++ js/task-06.js
@@ -3,3 +3,37 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+function createBoxes(amount) {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = '';
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.getElementById("boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.querySelector("button[data-create]").addEventListener("click", () => {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.value);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100.");
+  }
+});
+
+document
+  .querySelector("button[data-destroy]")
+  .addEventListener("click", destroyBoxes);
