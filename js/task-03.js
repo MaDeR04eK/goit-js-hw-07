06ea559b86246c7271949829e45b3a2c92fe240a
Kyryl <MaDeR04eK@users.noreply.github.com>
2023-12-18 17:04:31 +0100

    update task-3

--- js/task-03.js
+++ js/task-03.js
@@ -1,8 +1,8 @@
-const inputText = document
-  .getElementById("name-input")
-  .addEventListener("input", function () {
-    const inputValue = this.value.trim();
-    const outputText = document.getElementById("name-output");
+const inputText = document.getElementById("name-input");
+inputText.addEventListener("input", () => {
+  const inputValue = inputText.value.trim();
+  const outputText = document.getElementById("name-output");
 
-    outputText.textContent = inputValue === "" ? "Anonymous" : inputValue;
-  });
+  outputText.textContent = inputValue === "" ? "Anonymous" : inputValue;
+});
+console.log(inputText);
